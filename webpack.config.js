6a14bf1985a7b6813cb45e9df1b42724758668f8
Kazumi Inada <timer.ground.gm@gmail.com>
2018-06-28 20:08:47 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+
+const path = require( 'path' );
+
+const HTMLWebpackPlugin = require( 'html-webpack-plugin' );
+
+module.exports = {
+
+  context: path.resolve( __dirname, 'src' ),
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+
+  entry: './js/index.js',
+  output: {
+
+    path: path.resolve( __dirname, 'dist' ),
+    filename: 'bundle.js',
+
+  },
+
+  module: {
+
+    rules: [
+
+      {
+
+        test: /\.html$/,
+        use: 'html-loader',
+
+      },
+
+      {
+
+        test: /\.scss$/,
+        use: [
+
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' },
+
+        ],
+
+      }
+
+    ]
+
+  },
+
+  plugins: [
+
+    new HTMLWebpackPlugin( {
+
+      template: './index.html'
+
+    } )
+
+  ],
+
+};
